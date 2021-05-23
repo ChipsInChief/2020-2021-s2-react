b3aec8c59d2400f875612ebf17a7745a9d3838b5
Benjamin GUILLAUME <benjamin.guillaume@syspertec.com>
2021-05-23 11:22:25 +0200

    Création d'une première fonction pure

---
+++ memory/src/CoolComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function CoolComponent() {
+    return React.createElement('p', {}, 'Youpi so Cool!');
+}
--- memory/src/index.js
+++ memory/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CoolComponent } from './CoolComponent';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement(CoolComponent),
   document.getElementById('root')
 );
 
