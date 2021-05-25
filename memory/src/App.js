6b90f9fd81c7a7380cc0334531538a19d6b7c2e7
Benjamin GUILLAUME <benjamin.guillaume@syspertec.com>
2021-05-25 21:17:15 +0200

    Première version de memory avec un plateau de cartes "en dur" 💪🏻

--- memory/src/App.js
+++ memory/src/App.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react'
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import GuessCount from "./GuessCount";
+import Card from './Card'
 
 class App extends Component {
   render() {
-    return <div className="memory" />
+    return (
+      <div className="memory">
+        <GuessCount guesses={0} />
+        <Card card="😀" feedback="hidden" />
+        <Card card="🎉" feedback="justMatched" />
+        <Card card="💖" feedback="justMismatched" />
+        <Card card="🎩" feedback="visible" />
+        <Card card="🐶" feedback="hidden" />
+        <Card card="🐱" feedback="justMatched" />
+      </div>
+    );
   }
 }
 
---
+++ memory/src/Card.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Card.css'
+
+const HIDDEN_SYMBOL = '❓'
+
+const Card = ({ card, feedback }) => (
+  <div className={`card ${feedback}`}>
+    <span className="symbol">
+      {feedback === "hidden" ? HIDDEN_SYMBOL : card}
+    </span>
+  </div>
+);
+
+export default Card
+
+// Ancienne méthode de définition des fonctions JavaScript
+// function Card({ card, feedback }) {
+//   return (
+//     <div className={`card ${feedback}`}>
+//       <span className="symbol">
+//         {feedback === "hidden" ? HIDDEN_SYMBOL : card}
+//       </span>
+//     </div>
+//   );
+// }
