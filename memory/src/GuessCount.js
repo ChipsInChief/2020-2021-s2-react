f59593e30cba5850bffdc66ba18b536db58228d8
ChipsInChief <ouri3l@gmail.com>
2021-06-22 20:47:48 +0200

    Mise en place d'un validateur pour rendre obligatoire la prop "guesses". Cf. erreur dans la console (F12)

--- memory/src/GuessCount.js
+++ memory/src/GuessCount.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./GuessCount.css";
+import PropTypes from "prop-types";
 
 const GuessCount = ({ guesses }) => <div className="guesses">{guesses}</div>;
 
-GuessCount.defaultProps = {
-  guesses: 0,
+GuessCount.propTypes = {
+  guesses: PropTypes.number.isRequired,
 };
 
 export default GuessCount;
